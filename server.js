0b5fa0fae0401844cfde95fa44bb3db6fb36c0b2
bisrat <bisrat.amtataw@gmail.com>
2022-07-05 23:31:56 -0400

    heroku

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express')
 const mongoose = require ('mongoose') 
 const cors = require('cors');
 const morgan = require('morgan');
-// const models = require('./models')
-// const controllers = require ('./Controllers')
 const app = express()
 const MONGODB_URL = process.env
 
